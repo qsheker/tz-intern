fa1f00e12c7dcff18d799435b8108352e5f1d38c
Aldiyar <aldikzhaks@gmail.com>
2025-11-21 15:24:32 +0500

    initial commit

---
+++ src/main/java/org/qsheker/internpracticetask/InternPracticeTaskApplication.java
@@ -0,0 +1,13 @@
+package org.qsheker.internpracticetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternPracticeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternPracticeTaskApplication.class, args);
+    }
+
+}
